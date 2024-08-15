7e1c9f2d43363a4ebd20123be6538ad9dbb270d9
Raghav Gupta <raghav.k.gupta123@gmail.com>
2024-08-15 23:02:19 +0530

    Landing page with news

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Team from "./components/Team"
+import News from "./components/News"
 
 function App() {
   return (
     <main>
       <Header />
+      <News />
       <Team /> 
       <Footer />
     </main>
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,6 +1,6 @@
 export default function Team() {
   return (
-    <section className="bg-black mt-32 text-white p-4 text-center">
+    <section className="bg-black text-white p-4 text-center">
         <div>
             <h2>Team</h2>
             <p>Our team is made up of the best engineers and designers from around the world.</p>
